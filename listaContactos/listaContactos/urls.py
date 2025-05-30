daec6e144f888467047532b6e4dcf1e37242243b
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-29 21:45:46 -0500

    registrando la url de la nueva pagina de inicio

--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inicio.views import pagina_inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', pagina_inicio, name='pagina_inicio'),
 ]
