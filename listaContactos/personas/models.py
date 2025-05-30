6ae1326974d46b2bce0cbc239437c675d9d4ef66
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-29 21:12:35 -0500

    agregando un nuevo campo al modelo y asignandole un valor vacio para que no tenga errores con los datos que tenia antes, los cuales no tenian este campo

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -5,4 +5,5 @@ class Persona(models.Model):
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
     edad = models.IntegerField()#max_digits=3
+    email = models.EmailField(null=True, blank=True)
 
