eb4bce51b8195f74eb62c5a84cb205ed7e83410e
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-29 21:29:13 -0500

    modificando para que los campos nombres y apellidos null y blank sean obligatorios, para que no tengan problemas con la base de datos ni con el formulario, ademas se agrego el campo activo, para que sirva de ejemplo

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=100)
+    nombres = models.CharField(max_length=100, null=False, blank=False)
+    apellidos = models.CharField(max_length=100, null=False, blank=False)
     edad = models.IntegerField()#max_digits=3
-    email = models.EmailField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True) #afecta a la base de datos y al formulario
+    activo = models.BooleanField(default=True) #por defecto es True, si no se especifica se guarda como True
 
