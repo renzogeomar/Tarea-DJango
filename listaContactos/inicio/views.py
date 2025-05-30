4091337262808df2f006ad39ef5d3f31d9b58868
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-29 22:05:18 -0500

    entendiendo los argumentos de la funcion pagina_inicio, ademas mostrandolos en consola

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def pagina_inicio(*args, **kwargs):
+def pagina_inicio(request, *args, **kwargs):
+    print("Args:", args)
+    print("Kwargs:", kwargs)
+    print("Usuario:", request.user)
     return HttpResponse('<h1>Bienvenido a la lista de contactos</h1>')
 def anotherView(request):
     return HttpResponse('<h1>Solo otra página</h1>')
