ebfce598f8a96008de1af016be9f174280bf0e9f
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-29 21:54:11 -0500

    agregando más vistas, por ejemplos another y another2

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def pagina_inicio(*args, **kwargs):
     return HttpResponse('<h1>Bienvenido a la lista de contactos</h1>')
+def anotherView(request):
+    return HttpResponse('<h1>Solo otra página</h1>')
+def anotherView2(request):
+    return HttpResponse('<h1>Esta es otra página de prueba</h1>')
