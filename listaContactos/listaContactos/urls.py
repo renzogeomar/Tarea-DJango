cc6f1296655358dd3df15a5c8b714acea8fd3f13
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-06-04 23:05:26 -0500

    Renderizando una Persona de la app personas

--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import pagina_inicio, anotherView, anotherView2
+from personas.views import personaTestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pagina_inicio, name='pagina_inicio'),
     path('another/', anotherView),
     path('another2/', anotherView2),
+    path('personas/', personaTestView, name='otro'),
 ]
